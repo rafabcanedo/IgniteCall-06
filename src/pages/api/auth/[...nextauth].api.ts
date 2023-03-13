97f2d26f1445b12728cca535e8ca6f03b4a93ae4
Rafabcanedo <rafaitecanedo@gmail.com>
2023-03-13 16:46:00 -0300

    building form hook

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,8 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { signIn } from "next-auth/react";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import { PrismaAdapter } from "../../../lib/auth/prisma-adapter";
 
-export const authOptions: NextAuthOptions = {
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+ return{
+  adapter: PrismaAdapter(req, res),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -12,6 +19,15 @@ export const authOptions: NextAuthOptions = {
           scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
         }
       },
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          username: '',
+          email: profile.email,
+          avatar_url: profile.picture,
+        }
+      }
     }),
   ],
 
@@ -25,7 +41,17 @@ export const authOptions: NextAuthOptions = {
 
       return true
     },
+
+    async session({ session, user }) {
+      return {
+        ...session,
+        user,
+      }
+    },
   },
+ }
 };
 
-export default NextAuth(authOptions);
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return NextAuth(req, res, buildNextAuthOptions(req, res))
+}
